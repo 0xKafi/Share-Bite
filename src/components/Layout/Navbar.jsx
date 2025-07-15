3e65527382b51593a303a493f3dcd19057b8070e
Kafi <ahkafi05@gmail.com>
2025-07-16 01:16:45 +0600

    banner added

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -7,24 +7,24 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Menu } from 'lucide-react';
+import { Menu, MoveLeft, MoveRight } from 'lucide-react';
 
 const Navbar = () => {
     return (
         <div className='border border-base-100 mt-5 rounded-xl w-7/8 mx-auto'>
             <div className='mx-auto lg:p-4 p-5 flex items-center justify-between'>
                 <div>
-                    <p className='font-bold text-2xl'>ShareBite</p>
+                    <p className='font-bold text-2xl text-orange-500'>ShareBite</p>
                 </div>
                 {/* links*/}
                 <div className='space-x-8 hidden lg:block'>
-                    <NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to='/'>Home</NavLink>
-                    <NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to='/available-foods'>Available Foods</NavLink>
+                    <NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to='/'>Home</NavLink>
+                    <NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to='/available-foods'>Available Foods</NavLink>
 
                     <>
-                        <NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/add-food">Add Food</NavLink>
-                        <NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/manage-my-foods">Manage My Food</NavLink>
-                        <NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/my-food-request">My Food Request</NavLink>
+                        <NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/add-food">Add Food</NavLink>
+                        <NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/manage-my-foods">Manage My Food</NavLink>
+                        <NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/my-food-request">My Food Request</NavLink>
                     </>
                 </div>
 
@@ -39,12 +39,12 @@ const Navbar = () => {
                         <DropdownMenu>
                         <DropdownMenuTrigger><Menu></Menu></DropdownMenuTrigger>
                         <DropdownMenuContent>
-                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/">Home</NavLink></DropdownMenuItem>
-                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/available-foods">Available Foods</NavLink></DropdownMenuItem>
+                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/">Home</NavLink></DropdownMenuItem>
+                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/available-foods">Available Foods</NavLink></DropdownMenuItem>
                             <>
-                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/add-food">Add Food</NavLink></DropdownMenuItem>
-                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/manage-my-foods">Manage My Food</NavLink></DropdownMenuItem>
-                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium pb-1 border-b-1 border-black": ""} to="/my-food-request">My Food Request</NavLink></DropdownMenuItem>
+                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/add-food">Add Food</NavLink></DropdownMenuItem>
+                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/manage-my-foods">Manage My Food</NavLink></DropdownMenuItem>
+                            <DropdownMenuItem><NavLink className={({isActive})=> isActive ? "font-medium text-orange-500 pb-1 border-b-1 border-orange-500": ""} to="/my-food-request">My Food Request</NavLink></DropdownMenuItem>
                             </>
                         </DropdownMenuContent>
                         </DropdownMenu>
@@ -54,8 +54,8 @@ const Navbar = () => {
 
                 {/* login signup */}
                 <div className='space-x-2 hidden lg:block'>
-                    <Button>Login</Button>
-                    <Button variant="outline">Sign Up</Button>
+                    <Button variant="outline">Login</Button>
+                    <Button className="bg-orange-500 hover:bg-orange-400"><MoveRight></MoveRight> Sign Up</Button>
                 </div>
             </div>
         </div>
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <p>Home</p>
+        <div className='w-7/8 mx-auto container mt-5'>
+            <div className='flex lg:justify-between items-center pt-10'>
+                <div>
+                    <h1 className='font-bold text-6xl mb-2 leading-17'>Food Tastes Better<br /> <span className='text-orange-500'>When Shared.</span></h1>
+                    <p>Invite friends, split meals, and share your favorite bites — all in one place.</p>
+
+                </div>
+                <div>
+                    <img className='w-180' src="https://i.ibb.co/Zz2Qwv43/image.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
