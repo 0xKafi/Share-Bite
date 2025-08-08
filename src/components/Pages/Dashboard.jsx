2a711d815291974f0e92c6e78bf0d5fabb88ef4b
Kafi <ahkafi05@gmail.com>
2025-08-09 00:52:28 +0600

    aso animation added

---
+++ src/components/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className='mx-auto max-w-5xl min-h-screen bg-gray-50'>
+            <p>dashboard</p>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/components/Sections/Work.jsx
+++ src/components/Sections/Work.jsx
@@ -1,5 +1,8 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Work = () => {
   const steps = [
@@ -22,11 +25,11 @@ const Work = () => {
 
   return (
         <div className="my-30">
-            <h2 className="text-3xl font-bold text-center my-20">How It Work</h2>
+            <h2 data-aos="fade-up" data-aos-duration="1000" className="text-3xl font-bold text-center my-20">How It Work</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl mx-auto my-10 px-4">
             {
                 steps.map((step) => (
-                <Card key={step.id} className="relative p-4 pt-10">
+                <Card  data-aos="fade-up" data-aos-duration="1000" key={step.id} className="relative p-4 pt-10">
                 <div className="absolute -top-4 left-4 bg-primary text-white font-bold rounded-full w-8 h-8 flex items-center justify-center shadow-md">
                     {step.id}
                 </div>
